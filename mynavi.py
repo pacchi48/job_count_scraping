bd2d56ee7f6e92a6a2686a977a3b7bd312562fc5
pacchi48 <usk991013@gmail.com>
2025-03-12 09:33:01 +0900

    update:マイナビを小カテゴリまで拾うように修正

--- mynavi.py
+++ mynavi.py
@@ -5,62 +5,83 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re  # 正規表現ライブラリをインポート
 from selenium.webdriver.chrome.options import Options
-
-# モバイルエミュレーションの設定
-mobile_emulation = {
-    "deviceMetrics": {"width": 375, "height": 812, "pixelRatio": 3.0},
-    "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
-}
-
-chrome_options = Options()
-chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 
 # Chrome WebDriverのセットアップ
 cService = webdriver.ChromeService(executable_path="venv/lib/python3.13/site-packages/chromedriver_binary/chromedriver")
-driver = webdriver.Chrome(service=cService, options=chrome_options)
+driver = webdriver.Chrome(service=cService)
 
 # 指定したURLにアクセス
-driver.get('https://tenshoku.mynavi.jp/')
+driver.get('https://tenshoku.mynavi.jp/list/')
 
 # 全てのクラス名を含むCSSセレクタを使ってボタンを見つける
-button = driver.find_element(By.CSS_SELECTOR, ".topSearchPanel.renew.js__slideTrigger--jobtop")
+button = driver.find_element(By.CSS_SELECTOR, ".btnAddS.js__jobCheckbox")
 button.click()  # ボタンをクリック
 
-# # ページが完全に読み込まれるのを待つ
-# WebDriverWait(driver, 10).until(
-#     EC.presence_of_all_elements_located((By.CLASS_NAME, 'navItemList__item'))
-# )
-
-time.sleep(1)  # 1秒間プログラムを停止
+time.sleep(2)  # 1秒間プログラムを停止
 
 # アコーディオンのトリガー要素をすべて見つける
-triggers = driver.find_elements(By.CSS_SELECTOR, ".js__accordion--trigger")
+modal = driver.find_element(By.CSS_SELECTOR, ".modalChoice__content")
+categories = modal.find_elements(By.CSS_SELECTOR, ".modalChoice__item")
 
-print(len(triggers))
+data  =[]
 
-# それぞれのトリガーをクリックしてアコーディオンを展開
-for trigger in triggers:
-    # 親要素（.toggle）を取得し、`data-large-nm` 属性があるか確認
-    parent_div = trigger.find_element(By.XPATH, "./ancestor::div[contains(@class, 'toggle')]")
-    if parent_div.get_attribute("data-large-nm"):
-        trigger.click()
-        time.sleep(1)  # 各クリックの間に少し待つ
-
-# データの抽出
-data = []
-categories = driver.find_elements(By.CSS_SELECTOR, ".js__sliedeSearch--large")
 for category in categories:
-    category_name = category.find_element(By.CSS_SELECTOR, ".text").text
-    items = category.find_elements(By.CSS_SELECTOR, ".checkBoxList__item")
-    for item in items:
-        job_title = item.find_element(By.CSS_SELECTOR, ".checkBoxList__text").text
-        job_count = item.find_element(By.CSS_SELECTOR, ".textCaution").text.replace('件', '').replace(',', '')
-        data.append([category_name, job_title, int(job_count)])
+    # 大カテゴリ名
+    l_cate = category.text
+
+    # 大カテゴリをクリック
+    category.click()
+
+    # 画面遷移待機
+    time.sleep(2)
+
+    # 中カテゴリ群
+    # element = modal.find_element(By.CSS_SELECTOR, '.js__selectCondition--large[style="display: block;"]')
+
+    element = modal.find_element(By.XPATH, "//*[contains(@class, 'js__selectCondition--large') and not(contains(@style, 'display: none'))]")    
+    sections = element.find_elements(By.TAG_NAME, 'section')
+
+    for section in sections:
+        m_el = section.find_element(By.CSS_SELECTOR, ".choiceContent__sectionTitle")
+        # 中カテゴリ名
+        m_cate = m_el.find_element(By.CLASS_NAME, 'checkbox__text').text
+
+        # 小カテゴリ群
+        s_elements = section.find_elements(By.CSS_SELECTOR, ".choiceContent__item")
+
+        for s_el in s_elements:
+           # 小カテゴリ名
+           s_cate = s_el.find_element(By.CSS_SELECTOR, ".checkbox__text").text
+
+           # チェックボックスをクリック
+           s_el = s_el.find_element(By.TAG_NAME, "label")
+           # 要素がビューポート中央に来るようにスクロール
+           driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", s_el)
+           s_el.click()
+
+           # 内容を反映するボタンを押下
+           ref_button = driver.find_element(By.CSS_SELECTOR, ".modalChoice__submit").find_element(By.CSS_SELECTOR, ".btnSecondaryL.js__modal--apply")
+           ref_button.click()
+
+           # 画面遷移待機
+           time.sleep(1)
+
+           # 件数取得
+           count = driver.find_element(By.CSS_SELECTOR, ".btnSearch").find_element(By.CSS_SELECTOR, ".js__searchRecruit--count").text
+           data.append([l_cate, m_cate, s_cate, count])
+
+           # 職種ボタンを押下し、モーダルを開く
+           button.click()
+
+           time.sleep(2)  # 1秒間プログラムを停止
+
+           # チェックボックスをクリック
+           driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", s_el)
+           s_el.click()
 
 # DataFrameの作成
-df = pd.DataFrame(data, columns=['Category', 'Job Title', 'Job Count'])
+df = pd.DataFrame(data, columns=['L', 'M', 'S', 'Count'])
 
 df_transposed = df.T
 
